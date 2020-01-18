03c7081f7cab7801811294e8f31388f4c1377d23
Darren Cavell <darren_cavell@yahoo.com>
2020-01-18 18:56:29 +0700

    FE 20 - fix on webpack prod configuration

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,13 +4,6 @@ const HtmlWebPackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
-const dotenv = require('dotenv')
-
-const env = dotenv.config().parsed;
-const envKeys = Object.keys(env).reduce((prev, next) => {
-  prev[`process.env.${next}`] = JSON.stringify(env[next]);
-  return prev;
-}, {});
 
 module.exports = {
   entry: ['babel-polyfill', './src/index.js'],
